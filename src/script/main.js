bac933dfbd9eea8d058566258d3be179baa1d72d
Basel Akasha <baselakasha@gmail.com>
2019-01-02 15:17:00 +0200

    [#13] fix input validation when user leave empty input

--- src/script/main.js
+++ src/script/main.js
@@ -10,31 +10,34 @@ function ftc(val){ // fahrenheit to celsius
 function ktc(val){ // kelvin to celsius 
     return (Number(val)-273);
 }
-function warring(elem){ // empty all input and 
+function warring(elem){ 
     elem.style.border="2px solid red";
-    setTimeout(function(){
+    elem.addEventListener("input", function(){
         elem.style.border="none";
-        celsius.value="";
-        fahrenheit.value="";
-        kelvin.value="";
-    },1500);
+    });
 }
 window.onload=function(){
     "use strict";
     var celsius=document.getElementById("celsius"),
         fahrenheit=document.getElementById("fahrenheit"),
         kelvin=document.getElementById("kelvin");
+    
     celsius.addEventListener("input",function(){
         setTimeout(function(){
             if(celsius.value!="" && !isNaN(celsius.value)){
                 fahrenheit.value=ctf(celsius.value); 
                 kelvin.value=ctk(celsius.value);       
             }
+            else if(celsius.value.length==0){
+                fahrenheit.value=""; 
+                kelvin.value="";    
+            }
             else{
 	             warring(celsius);
             }
-        },350);
+        },100);
     });
+    
     fahrenheit.addEventListener("input",function(){    
        setTimeout(function(){
             if(fahrenheit.value!="" && !isNaN(fahrenheit.value)){
@@ -42,17 +45,26 @@ window.onload=function(){
                 celsius.value=ftcVal;
                 kelvin.value=ctk(ftcVal);
             }
+            else if(fahrenheit.value.length==0){
+                kelvin.value=""; 
+                celsius.value="";
+            }
             else{
 	             warring(fahrenheit);
             }
         },350);      
     });
+    
     kelvin.addEventListener("input",function(){
         setTimeout(function(){
             if(kelvin.value!=""&&!isNaN(kelvin.value)){
                var celsiusValue=ktc(kelvin.value);
                celsius.value=celsiusValue;
                fahrenheit.value=ctf(celsiusValue);
+            }
+            else if(kelvin.value.length==0){
+                celsius.value="";
+                fahrenheit.value="";
             }
             else{
 	             warring(kelvin);
